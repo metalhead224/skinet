d60c526f1ef4eec045d665ed187eabe83fb9c4a2
suman osti <suman.osti401@gmail.com>
2023-07-08 10:34:44 +0545

    handled http response

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    NavBarComponent
+  ],
   imports: [
     BrowserModule, 
     AppRoutingModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
